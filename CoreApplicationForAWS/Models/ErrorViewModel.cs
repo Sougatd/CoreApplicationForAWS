50f09f83aae1ce7495bffa9d7e30540a4e6f4c10
sougat.debnath <sougat.debnath@SOUGATD>
2023-12-13 16:24:54 +0530

    Add project files.

---
+++ CoreApplicationForAWS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreApplicationForAWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
